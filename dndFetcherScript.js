28e839b29fd89f180534b791e73bd4ec68ece017
Joseph Paulson <josethpaulson@gmail.com>
2022-10-13 04:52:45 +0000

    added fetch logic

--- dndFetcherScript.js
+++ dndFetcherScript.js
@@ -1,1 +1,18 @@
-console.log("yo")
+/* global fetch */
+
+console.log("yo");
+
+document.getElementById("submitButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    const value = document.getElementById("monsterInput").value;
+    if (value === "") return;
+    var modifiedValue = value.toLowerCase().split(" ").join("-");
+    const url = "https://api.open5e.com/monsters/" + modifiedValue + "/?format=json";
+    console.log(url);
+    fetch(url).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        console.log(json);
+        // insert stuff here
+    });
+});
